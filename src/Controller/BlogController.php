52a7e0b47bb4a9eef15f6079872e589a2d7832bc
devMaxd <maxime.doudnikoff@etu.univ-st-etienne.fr>
2025-12-09 21:49:02 +0100

    09/12/2025 propre

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,15 +4,39 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class BlogController extends AbstractController
+class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
+    // Page d'accueil (DOIT être en Anglais )
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'nom' => 'VOTRE_NOM',         // [cite: 45]
+            'prenom' => 'VOTRE_PRENOM',   // [cite: 45]
+            'groupe' => 'BUT1 RT - G1',   // [cite: 45]
         ]);
+    }
+
+    // Page CV (DOIT être en Français [cite: 77])
+    #[Route('/cv', name: 'app_cv')]
+    public function cv(): Response
+    {
+        return $this->render('/cv.html.twig');
+    }
+
+    // Page E-Portfolio (DOIT être en Français [cite: 77])
+    #[Route('/portfolio', name: 'app_portfolio')]
+    public function portfolio(): Response
+    {
+        return $this->render('/portfolio.html.twig');
+    }
+
+    // Page Loisirs (liée au container de l'accueil [cite: 47])
+    #[Route('/hobbies', name: 'app_hobbies')]
+    public function hobbies(): Response
+    {
+        return $this->render('/hobbies.html.twig');
     }
 }
